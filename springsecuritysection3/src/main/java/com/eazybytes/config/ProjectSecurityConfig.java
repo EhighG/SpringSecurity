d18ad248c0ac32e276f3797877327cbc7e7cd2a7
Ehigh <rkd2rb@naver.com>
2023-12-17 14:55:05 +0900

    깃 캐시 reset / ~jdbc 연결, ddl 파일 추가

--- springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.eazybytes.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -30,47 +37,52 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    /**
-     * manage multi-user info(inMemory)
-     * @return
-     */
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
+//    /**
+//     * manage multi-user info(inMemory)
+//     * @return
+//     */
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+////        /*
+////         Approach 1. use withDefaultPasswordEncoder()
+////         deprecated지만, 지금은 Inmemory 저장소를 사용하므로 괜찮음
+////         주의 : password가 코드에 드러나는 건 테스트 목적 외에는 사용 시 주의!
+////         */
+////        // UserDetails는 인터페이스, User는 구현 클래스 / 모두 스프링 시큐리티가 제공
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("123")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("123")
+////                .authorities("read")
+////                .build();
+//
 //        /*
-//         Approach 1. use withDefaultPasswordEncoder()
-//         deprecated지만, 지금은 Inmemory 저장소를 사용하므로 괜찮음
-//         주의 : password가 코드에 드러나는 건 테스트 목적 외에는 사용 시 주의!
+//         Approach 2. 빈으로 등록된 passwordEncoder 사용
+//         passwordEncoder 빈을 등록하는 메소드를 외부에 따로 만든다.
 //         */
-//        // UserDetails는 인터페이스, User는 구현 클래스 / 모두 스프링 시큐리티가 제공
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//        UserDetails admin = User.withUsername("admin")
 //                .password("123")
 //                .authorities("admin")
 //                .build();
 //
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("123")
 //                .authorities("read")
 //                .build();
-
-        /*
-         Approach 2. 빈으로 등록된 passwordEncoder 사용
-         passwordEncoder 빈을 등록하는 메소드를 외부에 따로 만든다.
-         */
-        UserDetails admin = User.withUsername("admin")
-                .password("123")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withUsername("user")
-                .password("123")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -79,4 +91,16 @@ public class ProjectSecurityConfig {
          */
         return NoOpPasswordEncoder.getInstance();
     }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth, DataSource ds) throws Exception {
+//        String userQuery = "custom Query";
+//        String authQuery = "custom Query";
+//
+//        auth.jdbcAuthentication()
+//                .dataSource(ds)
+//                .rolePrefix("ROLE_")
+//                .usersByUsernameQuery(userQuery)
+//                .authoritiesByUsernameQuery(authQuery);
+//    }
 }
